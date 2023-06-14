61cdd0eab99e22d4c3956b6a2022413f378c310d
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-06-13 20:15:38 -0400

    removed stuf that wasnt required making my life easier and removed those two functions that were added eearlier also all the questions are being asked in terminal without a hitch so yipee

--- develop/index.js
+++ develop/index.js
@@ -5,8 +5,7 @@ const inquirer = require('inquirer');
 const questions = [
   { Title: 'What is the title of your project ?' },
   { Descr: 'Provide a description of your project..' },
-  { Table: 'Would you like to provide a table of contents ?' },
-  { TableCont: 'What would you like to add to your table of contents ?' },
+  { TableCont: 'What would you like to add to your table of contents ? If Not Leave Leave Blank' },
   { Install: 'How do you install this application ?' },
   { Credit: 'Who/What do you credit for creating this app ?' },
   { Badges: 'Add Any Badge You May Have by pasting its link !' },
@@ -19,7 +18,6 @@ const questions = [
 const [
   { Title: title },
   { Descr: description },
-  { Table: tableOfContents },
   { TableCont: tableOfContentsContent },
   { Install: installation },
   { Credit: credits },
@@ -30,9 +28,19 @@ const [
   { Tests: test},
 ] = questions;
 
-const moreQuestions = () => {
-    inquirer.prompt([
-      {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: title,
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: description,
+      name: 'Description',
+    },
+    {
         type: 'input',
         message: tableOfContentsContent,
         name: 'TableContent',
@@ -88,121 +96,11 @@ const moreQuestions = () => {
         message: test,
         name: 'tests',
       },
-    ])
-    .then((answers) => {
-        fs.appendFile('bruh.txt', JSON.stringify(answers), (err) =>
-        err ? console.error(err) : console.log('Success!')
-      );
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
-      }
-    });
-  };
-
-const noTableOfContents = (answers) => {
-      inquirer.prompt([
-        {
-          type: 'input',
-          message: installation,
-          name: 'install',
-        },
-        {
-          type: 'input',
-          message: credits,
-          name: 'credits',
-        },
-        {
-          type: 'checkbox',
-          message: licenses,
-          choices: [
-            'None',
-            'Apache License 2.0',
-            'GNU General Public License v3.0',
-            'MIT License',
-            'BSD 2-Clause "Simplified" License',
-            'BSD 3-Clause "New" or "Revised" License',
-            'Boost Software License 1.0',
-            'Creative Commons Zero v1.0 Universal',
-            'Eclipse Public License 2.0',
-            'GNU Affero General Public License v3.0',
-            'GNU General Public License v2.0',
-            'GNU Lesser General Public License v2.1',
-            'Mozilla Public License 2.0',
-            'The Unlicense'
-          ],
-          name: 'licenses',
-        },
-        {
-          type: 'input',
-          message: Badges,
-          name: 'badges',
-        },
-        {
-          type: 'input',
-          message: Feature,
-          name: 'features',
-        },
-        {
-          type: 'input',
-          message: Contribute,
-          name: 'contribute?',
-        },
-        {
-          type: 'input',
-          message: test,
-          name: 'tests',
-        },
-      ])
-      .then((answers) => {
-        // Access user input for no table of contents scenario
-        fs.appendFile('bruh.txt', JSON.stringify(answers), (err) =>
-        err ? console.error(err) : console.log('Success!')
-      );
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
-        }
-      });
-    };
-          
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: title,
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: description,
-      name: 'Description',
-    },
-    {
-      type: 'checkbox',
-      message: tableOfContents,
-      choices: ['yes', 'no'],
-      name: 'Table',
-    }
   ])
   .then((answers) => {
-    fs.writeFile('bruh.txt', JSON.stringify(answers), (err) =>
-    err ? console.error(err) : console.log()
+    fs.writeFile('bruh.md', JSON.stringify(answers), (err) =>
+    err ? console.error(err) : console.log('written yipee!')
   );
-  if (answers.Table.includes('no')) {
-    noTableOfContents();
-  } else if (answers.Table.includes('yes')) {
-    moreQuestions();
-  } else {
-    return;
-  }
   })
   .catch((error) => {
     if (error.isTtyError) {
